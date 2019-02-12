a7dea0a012dfcd6fe8a0709598040e075007a614
Ryan Cotterell <ryan.cotterell@gmail.com>
2019-02-12 00:57:33 +0000

    gradient for psi broken

--- mrf.py
+++ mrf.py
@@ -77,7 +77,8 @@ class MRF(object):
             for i in V:
                 dphi[i] += p_V
                 for j in V:
-                    dpsi[i, j] += p_V
+                    if i != j:
+                        dpsi[i, j] += p_V
         return dphi, dpsi
 
     def fd(self, eps=1e-5):
